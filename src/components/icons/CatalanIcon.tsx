412ebaff276e6484bef5bb44dd7bed9e73785f76
Maksim Borissov <maksim.borissov@gmail.com>
2026-09-10 09:08:11 +0300

    moved flag to assets

--- src/components/icons/CatalanIcon.tsx
+++ src/components/icons/CatalanIcon.tsx
@@ -3,7 +3,7 @@ import type { ImgHTMLAttributes } from 'react'
 export function CatalanIcon(props: ImgHTMLAttributes<HTMLImageElement>) {
   return (
     <img
-      src="/images/flags/es-ct.svg"
+      src="/assets/flags/es-ct.svg"
       alt=""
       width={24}
       height={16}
